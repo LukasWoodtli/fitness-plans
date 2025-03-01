58f5b37d0a7ae663a2225e9f2188d12ce46a850e
Lukas Woodtli <woodtli.lukas@gmail.com>
2025-03-01 21:11:44 +0100

    Change Pull-Ups plan to use PDF as source

--- fitness_plans/pullup_challenge.py
+++ fitness_plans/pullup_challenge.py
@@ -1,39 +1,51 @@
 #!/usr/bin/env python3
+""""Taken from: https://twentypullups.com/"""
 from datetime import timedelta, date
-import requests
-from bs4 import BeautifulSoup
+from pathlib import Path
+
+import pymupdf
 
 from fitness_plans.lib.workout_calendar import CalendarEvent, WorkoutCalendar
 
 
 class Workout:
-    def __init__(self, week, title, subtitle):
+    def __init__(self, day, week, subtitle, sets_descriptions, sets):
+        self.day = day
         self.week = week
-        self.title = title
         self.subtitle = subtitle
+        self.sets_descriptions = sets_descriptions
         self.easy_set = []
         self.medium_set = []
         self.hard_set = []
+        for set in sets:
+            easy_set, medium_set, hard_set = set[1:]
+            self.easy_set.append(easy_set)
+            self.medium_set.append(medium_set)
+            self.hard_set.append(hard_set)
 
     def get_full_title(self):
-        return f"{self.title} (Week {self.week})"
+        return f"Day {self.day} (Week {self.week})"
 
     def _get_workout_set_text(self, level):
         text = f"{level.title()}:\n"
 
         if level.lower() == "easy":
             workout_set = self.easy_set
+            index = 0
         elif level.lower() == "medium":
             workout_set = self.medium_set
+            index = 1
         elif level.lower() == "hard":
             workout_set = self.hard_set
+            index = 2
         else:
             raise Exception(f"Level not known: {level}")
 
-        prerequisites = workout_set[0]
+        prerequisites = self.sets_descriptions[index]
+
         text += f"Prerequisites: {prerequisites}\n\n"
 
-        for i, s in enumerate(workout_set[1:], 1):
+        for i, s in enumerate(workout_set, 1):
             text += f"\tSet {i}: {s}\n"
         text += "\n\n"
 
@@ -42,78 +54,53 @@ class Workout:
     def get_description_text(self):
         text = f"{self.subtitle}\n\n"
         text += self._get_workout_set_text("Easy")
-        #text += self._get_workout_set_text("Medium")
-        #text += self._get_workout_set_text("Hard")
         return text
-
-    def __repr__(self):
-        text = self.get_full_title() + "\n"
-        text += self.get_description_text() + "\n"
-
-        return text
-
-    def append_workout_step(self, easy_workout, medium_workout, hard_workout):
-        self.easy_set.append(easy_workout)
-        self.medium_set.append(medium_workout)
-        self.hard_set.append(hard_workout)
 
 
 class PullUpChallenge:
 
-    def __init__(self, week_no=1, start_date=date.today(), workout_frequecy=timedelta(days=2)):
-        self._week_no = 1
+    def __init__(self, week_no=1, start_date=date.today(), workout_frequency=timedelta(days=2)):
+        self._week_no = week_no
         self.workout_start = start_date
-        self.workout_frequency = workout_frequecy
+        self.workout_frequency = workout_frequency
+        # PDF from: https://twentypullups.com/20-pull-ups_ilka-helo.pdf
+        self.pdf = Path(__file__).parent / "pullup_challenge" / "20-pull-ups_ilka-helo.pdf"
 
     def _get_all_workouts(self):
-        table_body = self._fetch_page_and_parse_table()
+        return self._read_pdf_and_parse_tables()
 
-        rows = table_body.find_all('tr')
-        all_workouts = []
-        for row in rows:
-            cols = row.find_all('td')
-            if not cols:
-                text = row.text
-                text = text.splitlines()
-                text = [line.strip() for line in text if line.strip()]
-                assert len(text) == 2
-                title = text[0]
-                assert title.startswith("Day")
-                subtitle = text[1]
-                assert subtitle.startswith("Rest")
-                workout_day = Workout(self._week_no, title, subtitle)
-                all_workouts.append(workout_day)
+    def _read_pdf_and_parse_tables(self):
+        pages = self._read_pdf()
+        return self._parse_tables(pages)
 
-            else:
-                assert workout_day
-                cols = [ele.text.strip() for ele in cols]
-                assert len(cols) == 3
-                workout_day.append_workout_step(*cols)
+    def _read_pdf(self):
+        with pymupdf.open(self.pdf) as doc:
+            for page in doc.pages():
+                text = page.get_text()
+                text = text.replace('© 2008 Olli Sikstus, Inspired by the Hundred Push Ups', '')
+                yield text
 
-        return all_workouts
-
-    def _fetch_page_and_parse_table(self):
-        page = self._fetch_page()
-        return self._parse_table(page)
-
-    def _fetch_page(self):
-        request = requests.get('https://twentypullups.com/')
-        soup = BeautifulSoup(request.text, 'html.parser')
-        return soup
-
-    def _parse_table(self, html_soup):
+    def _parse_tables(self, pdf_pages):
         week_no_text = f"Week {self._week_no}"
         # link to week
-        weeks = [i.get('href') for i in html_soup.find_all('a') if week_no_text in i.text]
+        week = [w for w in pdf_pages if w.startswith(week_no_text)]
+        assert len(week) == 1
+        week = week[0]
 
-        # parse table
-        week_page_request = requests.get(weeks[0])
-        week_page = BeautifulSoup(week_page_request.text, 'html.parser')
-
-        table = week_page.find('table')
-        table_body = table.find('tbody')
-
-        return table_body
+        days = week.split("Day")
+        week_no = days[0].split("\n")[0].replace("Week", "").strip()
+        week_no = int(week_no)
+        days = days[1:]
+        for day in days:
+            sets = day.split("\n")
+            sets = [s.strip() for s in sets if s.strip() != ""]
+            day_no = int(sets[0])
+            rest_description = sets[1]
+            sets_descriptions = sets[2:5]
+            sets = sets[5:]
+            entries_per_set = 4
+            sets = [sets[i:i + entries_per_set] for i in range(0, len(sets), entries_per_set)]
+            yield Workout(day_no, week_no, rest_description, sets_descriptions, sets)
 
     def _workouts_to_cal_events(self):
         workouts = self._get_all_workouts()
